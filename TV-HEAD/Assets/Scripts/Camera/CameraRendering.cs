18a0838063aa704a5e51d6d504bc9c7d3a21f10a
Kilian <allairekilian@gmail.com>
2020-01-31 20:16:32 +0100

    test

---
+++ TV-HEAD/Assets/Scripts/Camera/CameraRendering.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRendering : MonoBehaviour
+{
+    public Material mat;
+    Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.depthTextureMode = DepthTextureMode.DepthNormals;
+    }
+
+    // Update is called once per frame
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, mat);
+    }
+}
